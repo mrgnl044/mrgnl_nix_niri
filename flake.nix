c1152a5ca87a1c2878b7850edd76c5aba22f2f11
mrgnl <mrgnl@localhost>
2026-05-31 12:09:26 +0500

    add nix thinkpad profile

--- flake.nix
+++ flake.nix
@@ -3,14 +3,16 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    nixos_hardware.url = "github:NixOS/nixos-hardware/master";
   };
 
-  outputs = { self, nixpkgs, ... }: {
+  outputs = { self, nixpkgs, nixos_hardware, ... }: {
     nixosConfigurations.t14 = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
 
       modules = [
         ./host/t14/configuration.nix
+        nixos_hardware.nixosModules.lenovo-thinkpad
       ];
     };
   };
